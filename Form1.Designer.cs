2ce82a0b9ad5330d7215f707bb461066f61c5ae0
Emelie Erdbeer <drakoniamacro@gmail.com>
2020-01-28 21:00:59 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace LagSwitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(330, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game executable path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Drakonias Simple Lagswitch ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(329, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hotkey";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1000",
+            "2000",
+            "3000",
+            "4000",
+            "5000"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 164);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(329, 21);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Tag = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Lag duration (in milliseconds)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(278, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "This is just an example source you can modify it yourself ;)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 236);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LagSwitch
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("User32.dll")]
+        private static extern bool GetAsyncKeyState(ushort vKey);
+
+        private bool active = false;
+        private string gamepath;
+        private ushort hotkey;
+        private int duration;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.DataSource = Enum.GetValues(typeof(VirtualKeys)); //Add the virtual keys to the the combobox
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //We use OpenFileDialog() to find the path of our game executable, the textbox is set to ReadOnly
+            using (OpenFileDialog selectExecutable = new OpenFileDialog())
+            {
+                //Use this to filter acceptable input file types, you can find out how to do that yourself ;)
+                //selectExecutable.Filter = ""; 
+
+                if (selectExecutable.ShowDialog() == DialogResult.OK)
+                {
+                    gamepath = textBox1.Text = selectExecutable.FileName;
+                }
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Resolve hotkey string from our combobox to a ushort of the VirtualKeys enum
+            hotkey = (ushort)Enum.Parse(typeof(VirtualKeys), comboBox1.SelectedItem.ToString());
+
+            //Convert string from our combobox into int (Int32)
+            duration = Convert.ToInt32(comboBox2.SelectedItem);
+
+            //Check if our gamepath is longer than 0, you could additionally improve this by with File.Exists() to check if the filepath is valid
+            if (gamepath.Length == 0)
+            {
+                MessageBox.Show("No game executable path was set", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            //Toggle our bool
+            active = !active;
+
+            //Change button text according to state
+            if (active)
+            {
+                button2.Text = "stop";
+                timer1.Start();
+            }
+            else
+            {
+                button2.Text = "start";
+                timer1.Stop();
+            }
+        }
+
+
+        //Timer to check for hotkey press
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Check if our hotkey was pressed, you could improve this by using RegisterHotKey() and use events instead of a timer
+            if (GetAsyncKeyState(hotkey))
+            {
+                //This is the main path of our Lagswitch we are adding firewall rules to block traffic of our game executable
+                //This can obviously be improved a lot, just take a look at the arguments ;)
+                ProcessStartInfo AddRuleIn = new ProcessStartInfo("cmd.exe", "/c netsh dvfirewall firewall add rule name=\"UCLagSwitch\" dir=in action=block program=\"" + gamepath + "\" enable=yes");
+                ProcessStartInfo AddRuleOut = new ProcessStartInfo("cmd.exe", "/c netsh dvfirewall firewall add rule name=\"UCLagSwitch\" dir=out action=block program=\"" + gamepath + "\" enable=yes");
+                ProcessStartInfo DeleteRule = new ProcessStartInfo("cmd.exe", "/c netsh advfirewall firewall delete rule name=\"UCLagSwitch\" program=\"" + gamepath + "\"");
+
+                //Use WindowStyle Hidden to hide the command line windows
+                AddRuleIn.WindowStyle = ProcessWindowStyle.Hidden;
+                AddRuleOut.WindowStyle = ProcessWindowStyle.Hidden;
+                DeleteRule.WindowStyle = ProcessWindowStyle.Hidden;
+
+
+                //Add the firewall rules
+                Process.Start(AddRuleIn);
+                Process.Start(AddRuleOut);
+                Console.Beep();
+                //Sleep for the set duration
+                Thread.Sleep(duration);
+                //Delete the firewall rule
+                Process.Start(DeleteRule);
+            }
+        }
+    }
+}
